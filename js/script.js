0334c4164ba280abb58063c830449ae6891c04c1
nikkinbird <nikki.bollman@gmail.com>
2021-05-23 11:05:27 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var button = document.querySelector(".got-this");
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+
+var openModal = function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+};
+var closeModal = function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
+button.addEventListener("click", function () {
+  openModal();
+});
+
+modalX.addEventListener("click", function () {
+  closeModal();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
+    }
+  }
+});
